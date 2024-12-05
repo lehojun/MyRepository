d664970d4ae23d35e9081df4c72cb6423b46ce84
lehojun <hojunchris@naver.com>
2024-12-05 13:54:28 +0900

    Firs commit in dev branch

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -4,6 +4,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd Version");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
 	}
 
 }
